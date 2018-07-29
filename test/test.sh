115dc0158bef2146a250c95e6c98c195f9838896
suesh32 <suesh32@gmail.com>
2018-07-29 18:45:42 +0900

    Move test answer file to test/ans directory.

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 export LD_LIBRARY_PATH=../src:.
-./dumptest
+./dumptest > stdout
+diff stdout ans/stdout
 
 gdb ./dumptest core <<EOS > test.dmp 2>&1
 x/10s DUMP_AREA
 q
 EOS
 
-diff test.dmp test.dmp.org
+diff test.dmp ans/test.dmp
